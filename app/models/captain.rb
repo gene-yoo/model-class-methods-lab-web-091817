6a6c93f6716e4df2018eba033d5ed741082af282
gene-yoo <gene.y.yoo@gmail.com>
2017-10-07 19:46:50 -0400

    Add boat, captain, and classification class methods

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,28 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.joins(boats: :classifications).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    Captain.joins(boats: :classifications).distinct.where("classifications.name = 'Sailboat'")
+  end
+
+  def self.talented_seamen
+    motorboats = Captain.joins(boats: :classifications).where("classifications.name = 'Motorboat'")
+    sailboats = Captain.joins(boats: :classifications).where("classifications.name = 'Sailboat'")
+
+    seamen = motorboats.intersect(sailboats)
+
+    Captain.from(Captain.arel_table.create_table_alias(seamen, :captains))
+  end
+
+  def self.non_sailors
+    captains = Captain.all.distinct
+    sailors = self.sailors
+    non_sailors = captains - sailors
+
+    non_sailors_table = Captain.where(id: non_sailors.map(&:id))
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    Classification.all
+  end
+
+  def self.longest
+    longest_boat = Boat.all.order(length: :desc).limit(1)
+    Classification.joins(:boats).where("boats.id = ?", longest_boat.map(&:id))
+  end
 end
